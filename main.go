83f198e68cfe770010f4cd9a5a3aa3647ae9357e
Boburmirzo Kozimov <boburmirzo@sabgames.org>
2023-08-09 10:40:10 +0500

    Finishing first Static Project with Go

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not allowed", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, "hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parse error(): %v", err)
+		return
+	}
+	fmt.Println("Post Request Successful")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %v\n", name)
+	fmt.Fprintf(w, "Address = %v\n", address)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server at 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
